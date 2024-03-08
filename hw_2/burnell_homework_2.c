20cbe8bdfa2fe4ac74c220d75fa95a896c2e0373
DarsilRain <hales8torr@gmail.com>
2024-03-07 21:50:18 -0600

    begin hw 2

---
+++ hw_2/burnell_homework_2.c
@@ -0,0 +1,10 @@
+/******************************************************************************
+*    Authors: Haley Burnell, Angus Brennan, Addison Goforth
+*    Date: 7 March 2024
+*    Purpose: Learn about producers and consumers by modifying some example
+*           code.
+*           1) Producer inserts a string and prints to output
+*           2) consumer reverses the string and prints to output
+*
+*  Note Compile and run with> gcc burnell_homework_2.c -o prodcons && ./prodcons
+******************************************************************************/
