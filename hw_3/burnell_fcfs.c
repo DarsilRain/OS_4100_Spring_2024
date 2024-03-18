4ee52f34e37592e6e59658a0b74b0290fb082387
coursework <coursework@coursework.csc.tntech.edu>
2024-03-18 15:06:37 -0500

    program 3 seup

---
+++ hw_3/burnell_fcfs.c
@@ -0,0 +1,11 @@
+/******************************************************************************
+*    Authors: Haley Burnell, Angus Brennan, Addison Goforth
+*    Date: 18 March 2024
+*    Purpose: Learn about schedulers by implementing priority
+*             scheduling non pre-emptive algorithm
+*           Given: fcfs.c example as a starting point
+*           Input: burst time and priority of each process
+*           Output: print burst time, waiting time, and turnaround time
+*
+*  Note Compile and run with> gcc burnell_fcfs.c -o sched && ./sched
+******************************************************************************/
